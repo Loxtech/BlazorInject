d462c3e0dc96388b8be444c5be69547fb21d08cc
Loke Bendtsen <lokebendtsen@gmail.com>
2023-09-01 09:01:16 +0200

    Add project files.

---
+++ BlazorInject/Data/User.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorInject.Data
+{
+    public class User
+    {
+        public string Name = "Simon";
+        public string Job = "Programmering";
+    }
+
+    public class Identity
+    {
+        public string Name = "Bob";
+        public string Job = "Infrastructure";
+    }
+
+}
